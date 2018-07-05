b0374e012905348f2dad262ff76470f1162a40d6
lolcookie <dylan.broadbridge@gmail.com>
2018-07-06 02:38:08 +0800

    add dockerfile and script

---
+++ docker-run.sh
@@ -0,0 +1,12 @@
+
+# Change into the directory that contains this script.
+cd "$(dirname "$0")" || exit 1
+
+# Enter the virtual environment and map this repository
+# into the container at ~root/workshop.
+exec docker run                 \
+       --interactive            \
+       --tty                    \
+       -v "$PWD:/root/workshop" \
+       psimons/hex2017          \
+       "$@"
